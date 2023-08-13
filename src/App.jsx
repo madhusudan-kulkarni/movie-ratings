6dd841120a937bd8fea09a0b8c1150286c10603e
Madhusudan Kulkarni <96856144+madhusudan-kulkarni@users.noreply.github.com>
2023-08-13 11:03:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen ">
+      <div className="text-4xl text-center">
+        <p>Vite + React + Tailwind CSS 🚀</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
